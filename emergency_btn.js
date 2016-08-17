88a6c745d466c9e96e743b479f97a32c06d0fd5b
kathyorr7152 <katbatorr@gmail.com>
2016-08-17 12:42:02 -0700

    pushing safespace demo

---
+++ emergency_btn.js
@@ -0,0 +1,36 @@
+
+    $(document).ready(function(){
+    navigator.geolocation.getCurrentPosition(handle_geolocation_query);
+     });
+     
+     function handle_geolocation_query(position){
+     // do something with the position information
+     lat = parseInt(position.coords. latitude*10000,10)/10000;
+     lon = parseInt(position.coords.longitude*10000,10)/10000; 
+     alert(lat + " " + lon);
+     }
+    
+    $(function() {
+        var Geo={};
+
+        if (navigator.geolocation) {
+           navigator.geolocation.getCurrentPosition(success, error);
+        }
+
+        //Get the latitude and the longitude;
+        function success(position) {
+            Geo.lat = position.coords.latitude;
+            Geo.lng = position.coords.longitude;
+            populateHeader(Geo.lat, Geo.lng);
+        }
+
+        function error(){
+            console.log("Geocoder failed");
+        }
+
+        function populateHeader(lat, lng){
+            $('#Lat').html(lat);
+            $('#Long').html(lng);
+        }
+
+    });
